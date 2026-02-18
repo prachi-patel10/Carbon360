d47562348e85f20ddb6cff6e68671988c9ade077
Vinee Patel <167003304+vineepatel@users.noreply.github.com>
2026-02-18 18:21:34 +0530

    buttons changes

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideToastr } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,  // <-- Add this
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('ProjectApp');
