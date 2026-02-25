a83ebb753befe69940ccf3f26baf28307b19b8ae
Vinee Patel <167003304+vineepatel@users.noreply.github.com>
2026-02-25 15:12:51 +0530

    ui changes

---
+++ Backend/ProjectApp.Repository/Services/Masters/EmissionFactor/EmissionFactorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectApp.Repository.Services.Masters.EmissionFactor
+{
+    public class EmissionFactorService
+    {
+    }
+}
--- Frontend/src/app/private/fleet-transport/vehicle-ec/vehicle-service-ec.ts
+++ Frontend/src/app/private/fleet-transport/vehicle-ec/vehicle-service-ec.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { enviornment } from '../../../enviorments/enviornment';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class VehicleServiceEC {
-  
+export class TripService {
+
+  private apiUrl = enviornment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getVehicles(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/VehicleMaster/getalllist`);
+  }
+
+  getCities(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/City/All`);
+  }
+
+  addTrip(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/VehicleTrip/AddTrip`, data);
+  }
+
 }
