e8cbad459cf87eb59c568c9b8ed096e7d7b705eb
Prachi Patel <prachipatel1024@gmail.com>
2026-03-12 14:22:16 +0530

    Add WorkflowActionDTO

---
+++ Backend/ProjectApp.Core/DTOs/Common/WorkflowActionDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectApp.Core.DTOs.Common
+{
+    public class WorkflowActionDTO
+    {
+        public int WorkflowId { get; set; }
+        public int CurrentStatusId { get; set; }
+        public int NextStatusId { get; set; }
+        public string ActionName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
